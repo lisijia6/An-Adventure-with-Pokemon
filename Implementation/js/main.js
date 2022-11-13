f6b0420dd219adbb5648cd16f25976373ee92ee0
Sijia (Nancy) Li <lsjnancy@outlook.com>
2022-11-13 07:10:23 -0500

    modified index.html and main.js

--- Implementation/js/main.js
+++ Implementation/js/main.js
@@ -5,7 +5,11 @@ let dateParser = d3.timeParse("%Y-%m-%d");
 // (1) Load data with promises
 
 let promises = [
-
+    d3.csv("data/pokemonStats_basic.csv"),
+    d3.csv("data/films.csv"),
+    d3.csv("data/tvSeries.csv"),
+    d3.csv("data/videoGames.csv"),
+    d3.csv("data/pokemonCombats_basic.csv")
 ];
 
 Promise.all(promises)
@@ -22,7 +26,7 @@ function createVis(data) {
     let tvSeriesData = data[2]
     let videoGamesData = data[3]
     let battleData = data[4]
-    let pokemonGoData = data[5]
+    // let pokemonGoData = data[5]
 
     // error, perDayData, metaData
     // if(error) { console.log(error); }
@@ -37,6 +41,10 @@ function createVis(data) {
     // (4) Create visualization instances
     // *** TO-DO ***
     // Example: let countVis = new CountVis("countvis", allData);
+    pokemonCompareVis1 = new PokemonComparisonVis("pokemon-comparison-1",
+        "pokemon-comparison-1-img", "pokemon-comparison-1-name", pokemonStatsData, 1);
+    pokemonCompareVis2 = new PokemonComparisonVis("pokemon-comparison-2",
+        "pokemon-comparison-2-img",  "pokemon-comparison-2-name", pokemonStatsData,2);
 
     // *** TO-DO ***
     //  pass event handler to CountVis, at constructor of CountVis above
@@ -47,4 +55,15 @@ function createVis(data) {
     // *** TO-DO ***
     // eventHandler.bind("selectionChanged", function(event){ ...
 
+}
+
+// change comparison pokemon based on select box
+function pokemonChange(value) {
+    if (value === 1) {
+        selectedPokemon =  document.getElementById('pokemon-comparetest-1').value;
+        pokemonCompareVis1.wrangleData();
+    } else {
+        selectedPokemon =  document.getElementById('pokemon-comparetest-2').value;
+        pokemonCompareVis2.wrangleData();
+    }
 }
